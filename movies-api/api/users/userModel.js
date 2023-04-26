748dee9166664ef5910b7a749adfd762895a75e4
Aaron O' Brien <20093613@mail.wit.ie>
2023-04-26 10:36:05 +0100

    Mongo lab User Favourites

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -2,9 +2,20 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+//... Code as before
+
+const MovieSchema = new Schema({
+  id: Number,
+  title: String
+});
+
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  password: {type: String, required: true },
+  favourites: [MovieSchema]
 });
+
+// ..code as before
+
 
 export default mongoose.model('User', UserSchema);
