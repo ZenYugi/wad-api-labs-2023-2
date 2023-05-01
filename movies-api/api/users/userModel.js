b59be449dd26764fe39ed5f2ca58c2493f6c0132
Aaron O' Brien <20093613@mail.wit.ie>
2023-05-01 14:20:58 +0100

    Topic 12 - Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
+
 
 const Schema = mongoose.Schema;
 
@@ -14,6 +16,46 @@ const UserSchema = new Schema({
   password: {type: String, required: true },
   favourites: [MovieSchema]
 });
+
+UserSchema.methods.comparePassword = function (passw, callback) {
+  bcrypt.compare(passw, this.password, (err, isMatch) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isMatch);
+  });
+};
+
+
+//Find a user based on their username
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+
+//These updates to the User Schema define a pre-save hook that encrypts the password property before it is saved or updated. Also, the comparePassword() instance method can be used to authenticate users using encrypted passwords.
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+  if (this.isModified('password') || this.isNew) {
+      bcrypt.genSalt(10, (err, salt)=> {
+          if (err) {
+              return next(err);
+          }
+          bcrypt.hash(user.password, salt, null, (err, hash)=> {
+              if (err) {
+                  return next(err);
+              }
+              user.password = hash;
+              next();
+          });
+      });
+  } else {
+      return next();
+  }
+});
+
+
 
 // ..code as before
 
